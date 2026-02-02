adbdc5eee9d20f98af685061de1893e522c57ba0
EmanuelFadda <fadda.dev@gmail.com>
2026-02-02 22:36:53 +0100

    autentication with bluetooth and simple communication

--- captive_portal/captive_portal.ino
+++ captive_portal/captive_portal.ino
@@ -2,19 +2,19 @@
 #include <DNSServer.h>
 #include <AsyncTCP.h>
 #include "ESPAsyncWebServer.h"
+#include "BluetoothSerial.h"
 
+// DEVO USARE L'SD PER SALVARMI LE COSE IN MEMORIA
 
 // access point generato dall'esp
-char *ssid_esp="test";
-char *psw_esp="1234";
+char *ssid_esp ="test";
+char *psw_esp = "12345678";
 
+char *psw_bluetooth ="1234";
 
 DNSServer dnsServer;
-AsyncWebServer server(80);
-
-String email;
-String password;
-
+AsyncWebServer httpServer(80);
+BluetoothSerial SerialBT;
 
 const char index_html[] PROGMEM= R"rawliteral(
   <html>
@@ -27,14 +27,24 @@ const char index_html[] PROGMEM= R"rawliteral(
       <h1>GO AWAY, THIS IS A FAKE LOGIN!!!</h1>
       <p>Form used for debugging purpose.</p>
       <form action="/login">
-        Your email Google : <input type="email" name="email"></input>
+        Your email Google : <input type="email" name="email"></input><br>
         Password : <input type="password" name="password"></input>
-        <input type="submit">Login</input>
+        <input type="submit"></input>
       </form>
     </body>
   </html>
 )rawliteral";
 
+const char login_html[] PROGMEM=R"rawliteral(
+      <html>
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Login</title>
+      </head>
+      <body>        Don't send your data in unsafe logins and public wifi...<br> 
+        <a href="/">Turn back</a></body>
+      </html>
+      )rawliteral";
 
 class CaptiveRequestHandler : public AsyncWebHandler{
   public:
@@ -52,14 +62,12 @@ class CaptiveRequestHandler : public AsyncWebHandler{
 
 
 void setupServerPages(){
-  
-
   // si setta il comportamento per ogni singola pagina
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *r){
+  httpServer.on("/", HTTP_GET, [](AsyncWebServerRequest *r){
     r->send_P(200,"text/html",index_html);
   });
 
-  server.on("/login",HTTP_GET,[](AsyncWebServerRequest *r){
+  httpServer.on("/login",HTTP_GET,[](AsyncWebServerRequest *r){
     String inputEmail="";
     String inputPassword="";
 
@@ -72,51 +80,65 @@ void setupServerPages(){
 
     Serial.println("USER DATA: "+ inputEmail+ " "+ inputPassword);
 
-    r->send(200,"text/html",R"rawliteral(
-      <html>
-        Don't send your data in unsafe logins and public wifi...<br> 
-        <a href="/">Turn back</a>
-      </html>
-      )rawliteral");
+    r->send(200,"text/html",login_html);
   });
 
 
-  server.onNotFound([](AsyncWebServerRequest *r){
+  httpServer.onNotFound([](AsyncWebServerRequest *r){
     r->redirect("/");
   });
 }
 
 
 void setESPLocalNetwork(char* ssid,char* psw,IPAddress accesspoint,IPAddress gateway,IPAddress subnet){
-
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(accesspoint,gateway,subnet);
-  
   while (!WiFi.softAP(ssid,psw)){
     delay(750);
     Serial.println("...");
   }
-
   dnsServer.start(53, "*", WiFi.softAPIP());
-
-  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);
+  httpServer.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);
   setupServerPages();
-  server.begin();
-
+  httpServer.begin();
   Serial.println("ESP router IP "+WiFi.softAPIP().toString());
 }
 
 
+void authBLConnection(){
+  SerialBT.println("Need a password to access:");
+  bool ok=false;
+  while (!ok){
+    // lettura della stringa
+    String psw="";
+    while (SerialBT.available()==0){ 
+      delay(1000);
+    }
+
+    while (SerialBT.available()>0){
+      char ch=(char)SerialBT.read();
+      psw+=ch;
+    }
+
+    psw.trim();
+    if (psw==psw_bluetooth){
+      ok=true;      
+    }
+  }
+  SerialBT.println("User authenticated");
+}
+
 void setup() {
-  Serial.println("START");
   Serial.begin(115200);
-
-
-
-  // setESPLocalNetwork(ssid_esp,psw_esp,IPAddress(8,8,8,8), IPAddress(8,8,8,8), IPAddress(255,255,255,0));
+  Serial.println("START");
+  while (!SerialBT.begin("ESP32")){
+    delay(500);
+  }
+  authBLConnection();
+  setESPLocalNetwork(ssid_esp,psw_esp,IPAddress(8,8,8,8), IPAddress(8,8,8,8), IPAddress(255,255,255,0));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //dnsServer.processNextRequest();
+  dnsServer.processNextRequest();
+  delay(20);
 }
